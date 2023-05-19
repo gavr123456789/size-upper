32b6c0fb5662bdca489142855a886efb82b99665
gavr123456789 <gavr123456789@gmail.com>
2023-05-19 23:45:43 +0300

    init

---
+++ comic-recursive4.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# Define the directory containing the images
+# image_dir="/home/gavr/scale/origin"
+
+# Parse command line arguments
+while getopts "i:" opt; do
+  case ${opt} in
+    i )
+      image_dir=${OPTARG}
+      ;;
+    \? )
+      echo "Invalid option: -${OPTARG}" >&2
+      exit 1
+      ;;
+    : )
+      echo "Option -${OPTARG} requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+# Define the directory for small images
+small_dir_name="smalls"
+scaled_dir_name="scaled"
+
+# Loop through each subdirectory in the image directory
+for dir in "${image_dir}"/*/; do
+    # Get the directory name without the trailing "/"
+    dir=${dir%*/}
+    # Define the directory for small images
+    small_dir="${dir}/smalls"
+    scaled_dir="${dir}/scaled"
+    rm -rf "${small_dir}"
+    rm -rf "${scaled_dir}"
+    mkdir "${small_dir}"
+    mkdir "${scaled_dir}"
+    # Loop through each image in the directory
+    count=0 # initialize the count variable
+    total=$(ls "${dir}" | wc -l) # count the total number of images
+    for image in "${dir}"/*.{png,jpg,jpeg,webp}; do
+        # Get the image resolution using ImageMagick's identify command
+        resolution=$(identify -format "%wx%h" "${image}")
+        # Print the status message for the image
+        echo "Checking ${image} (resolution: ${resolution})..."
+        # Extract the width and height of the image
+        width=$(echo "${resolution}" | cut -d 'x' -f 1)
+        height=$(echo "${resolution}" | cut -d 'x' -f 2)
+        if [ "${width}" -lt 4000 ] && [ "${height}" -lt 5000 ]; then
+            # Move the image to the small directory
+            mv "${image}" "${small_dir}"
+            count=$((count+1)) # increment the count variable
+            # Print the status message for the moved image
+            echo "Moved ${count}/${total} images to ${small_dir}"
+        fi
+    done
+
+    # Check if the small directory is not empty before running the image scaling script
+    if [ "$(ls -A "${small_dir}")" ]; then
+        realesrgan-ncnn-vulkan -i "${small_dir}" -o "${scaled_dir}" -s 3 -f webp -j 8:8:8
+        for file in ${scaled_dir}/*; do cwebp -mt -q 70 "$file" -o "${file%.*}.webp"; done
+        cp "${scaled_dir}"/* "${dir}"
+    else
+        echo "No images to scale in ${small_dir}"
+    fi
+
+    rm -rf ${scaled_dir}
+    rm -rf ${small_dir}
+done
+
